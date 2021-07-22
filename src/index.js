ce7dc7c24e6b3a676855b20b5fe6e758e74e45ab
MarinaMuntian <dp060491smv@gmail.com>
2021-07-22 19:13:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,9 +10,32 @@ const DEBOUNCE_DELAY = 300;
 const searchInput = document.getElementById('search-box');
 const cardContainer = document.querySelector('.js-card-container');
 
+const renderCountryCard = (template, country) => {
+    const markup = template(country);
+    cardContainer.insertAdjacentHTML('beforeend', markup);
+
+};
+
+const isFetchSucces = (value) => {
+    if (value.length > 10) {
+        Notify.Failure('Too many matches found. Please enter a more specific name.');
+    }
+    if (value.length > 1 && value.length <= 10) {
+        renderCountryCard(countriesListMarkup, value);
+    }
+     if (value.length === 1) {
+        renderCountryCard(countryMarkup, value);
+     }
+    onFetchError();
+}
+
+const onFetchError = () => {
+    error(Notify.Failure('Oops, there is no country with that name'));
+}
+
 const onSearch = () => {
     cardContainer.innerHTML = ""
-    const searchedCountry = searchInput.value;
+    const searchedCountry = searchInput.target.value;
 
     if (searchedCountry === "")
     { return; }
@@ -20,10 +43,6 @@ const onSearch = () => {
     fetchCountries(searchedCountry)
         .then(isFetchSucces)
         .catch(onFetchError)
-}
- 
-const isFetchSucces = () => {
-    
 }
 
  searchInput.addEventListener('input', debounce(onSearch, 300));
